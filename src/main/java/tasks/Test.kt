3ca0bf87f76b18ef3b84b492f84165689a9c43ef
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-12-15 20:10:33 +0700

    Add Java file

--- src/main/java/tasks/Test.kt
+++
@@ -1,8 +0,0 @@
-package tasks
-
-fun main() {
-    for (i in 0..0) {
-        for (j in 0..0)
-        println(j)
-    }
-}
---
+++ src/main/java/tasks/TestJava.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
