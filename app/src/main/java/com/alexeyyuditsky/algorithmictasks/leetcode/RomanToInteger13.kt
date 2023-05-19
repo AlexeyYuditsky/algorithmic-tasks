bfc8123bd6b6c279646206eceac786264a1e2f85
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-05-19 08:16:29 +0300

    New solution for 13 task

--- app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/RomanToInteger13.kt
+++ app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/RomanToInteger13.kt
@@ -1,12 +1,28 @@
 package com.alexeyyuditsky.algorithmictasks.leetcode
 
 fun main() {
-    val res = romanToInt("MMM")
+    val res = romanToInt("XIV")
     println(res)
 }
 
 fun romanToInt(str: String): Int {
-    val map = mutableMapOf(
+    val map = mapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
+
+    var number = 0
+    var last = 1000
+
+    for (c in str) {
+        val value = map[c] ?: return 0
+        if (value > last) number -= last * 2
+        number += value
+        last = value
+    }
+
+    return number
+}
+
+fun romanToInt2(str: String): Int {
+    val map = mapOf(
         'I' to 1,
         'V' to 5,
         'X' to 10,
