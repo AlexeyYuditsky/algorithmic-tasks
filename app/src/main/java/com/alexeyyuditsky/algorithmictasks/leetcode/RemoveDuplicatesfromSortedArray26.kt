2072bb4cc5c740219cd9b3a5f3a5a1f1d8160f4b
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-06-13 08:42:17 +0300

    Solution 26 final

--- app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/RemoveDuplicatesfromSortedArray26.kt
+++ app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/RemoveDuplicatesfromSortedArray26.kt
@@ -4,22 +4,35 @@ package com.alexeyyuditsky.algorithmictasks.leetcode
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
  * */
 fun main() {
-    val intArray = intArrayOf(1, 1, 2, 2, 3)
-    val res = removeDuplicates(intArray)
+    val intArray = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)
+    val res = removeDuplicates2(intArray)
     println("k = $res")
     println("intArray = ${intArray.toList()}")
 }
 
+fun removeDuplicates2(nums: IntArray): Int {
+    var counter = 0
+
+    for (i in 1 until nums.size) {
+        if (nums[counter] != nums[i]) {
+            counter++
+            nums[counter] = nums[i]
+        }
+    }
+    return counter + 1
+}
+
 fun removeDuplicates(nums: IntArray): Int {
-
-    // если у элемента есть дубликат, то в месте где дубликат установить следующий элемент списка
-
-    for (i in nums.indices) { // 0
-        for (j in i + 1 until nums.size) { // 1
-
+    for (i in nums.indices) {
+        for (j in 1 until nums.size) {
+            if (nums[i] == nums[j]) nums[j] = Int.MAX_VALUE
+            if (nums[i] > nums[j]) {
+                val temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
         }
     }
 
-    println(nums.toList())
-    return nums.size
+    return nums.count { it != Int.MAX_VALUE }
 }
