1c06cb60ee6d5a9a916d80f33b1e092a58814bfc
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-07-26 09:11:14 +0300

    Solution 121 (new 3)

--- app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/BestTimetoBuyandSellStock121.kt
+++ app/src/main/java/com/alexeyyuditsky/algorithmictasks/leetcode/BestTimetoBuyandSellStock121.kt
@@ -4,18 +4,18 @@ package com.alexeyyuditsky.algorithmictasks.leetcode
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  * */
 fun main() {
-    val res = maxProfit(intArrayOf(5, 5, 1))
+    val res = maxProfit(intArrayOf(7, 1, 5, 3, 6, 4))
     println(res)
 }
 
 fun maxProfit(prices: IntArray): Int {
     var max = 0
     var min = prices[0]
-    for (i in 1 until prices.size) {
-        if (min < prices[i]) {
-            max = (prices[i] - min).coerceAtLeast(max)
+    prices.forEach {
+        if (min < it) {
+            max = if (it - min < max) max else it - min
         } else {
-            min = prices[i]
+            min = it
         }
     }
     return max
